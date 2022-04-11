d883a30fc88edc1c7ed554cd6e5b4821778561f2
binod164 <binod7_grg@hotmail.com>
2022-04-10 20:21:41 -0500

    add a route to create event

--- myapp/event_lists/views.py
+++ myapp/event_lists/views.py
@@ -5,3 +5,16 @@ from myapp.models import Event
 from myapp.event_lists.forms import EventForm
 
 event_lists = Blueprint('event_lists', __name__)
+
+@event_lists.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    form = EventForm()
+    if form.validate_on_submit():
+        event_post = Event(title=form.title.data, text=form.text.data, user_id=current_user.id)
+        db.session.add(event_post)
+        db.session.commit()
+        flash('Event Post Created')
+        print('Event post was created')
+        return redirect(url_for('core.index'))
+    return render_template('create_post.html', form=form)
