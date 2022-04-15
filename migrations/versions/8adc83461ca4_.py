c9e9a4c39d5bdb1f1754e3cb44aa7fac93213f2f
binod164 <binod7_grg@hotmail.com>
2022-04-15 15:12:10 -0500

    deploy

--- migrations/versions/8adc83461ca4_.py
+++ migrations/versions/8adc83461ca4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2a0c81874f69
+Revision ID: 8adc83461ca4
 Revises: 
-Create Date: 2022-04-13 15:04:50.598738
+Create Date: 2022-04-15 15:06:46.044455
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2a0c81874f69'
+revision = '8adc83461ca4'
 down_revision = None
 branch_labels = None
 depends_on = None
